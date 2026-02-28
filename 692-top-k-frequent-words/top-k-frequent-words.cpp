05250d236ed416df54edb93896345cf343babfad
niraj75 <143876990+Niraj-kumar-75@users.noreply.github.com>
2026-02-28 12:34:11 +0530

    Time: 3 ms (64.41%) | Memory: 18.2 MB (25.33%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+
+        // Step 1: Frequency map
+        unordered_map<string,int> freq;
+        for(string &s : words) {
+            freq[s]++;
+        }
+
+        // Step 2: Custom comparator
+        auto cmp = [](const pair<int,string>& a,
+                      const pair<int,string>& b) {
+            if(a.first == b.first)
+                return a.second > b.second; // string ASC (min first)
+            return a.first < b.first;       // freq DESC
+        };
+
+        // Step 3: Max Heap with custom comparator
+        priority_queue<pair<int,string>,
+                       vector<pair<int,string>>,
+                       decltype(cmp)> pq(cmp);
+
+        for(auto &it : freq) {
+            pq.push({it.second, it.first});
+        }
+
+        // Step 4: Pop k elements
+        vector<string> res;
+        while(k--) {
+            res.push_back(pq.top().second);
+            pq.pop();
+        }
+
+        return res;
+    }
+};
