12149c88f431053ea0747d3e67cad5b2520b954e
niraj75 <143876990+Niraj-kumar-75@users.noreply.github.com>
2026-06-17 08:36:50 +0530

    Time: 43 ms (84.01%) | Memory: 29.2 MB (46.16%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -16,6 +16,7 @@ public:
                     ans.push_back({nums[i],nums[left],nums[right]});
                     left++;
                     right--;
+                    //left duplicate ko skip kro
                     while (left < right && nums[left] == nums[left - 1]) {
                         left++;
                     }
@@ -24,6 +25,7 @@ public:
                     while (left < right && nums[right] == nums[right + 1]) {
                         right--;
                     }
+                
                 }
                 else if(sum<0)
                 {
