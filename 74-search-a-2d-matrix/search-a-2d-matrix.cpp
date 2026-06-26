c3f4835cd291979e99066ddfeac553859fedc7ce
niraj75 <143876990+Niraj-kumar-75@users.noreply.github.com>
2026-06-26 18:43:34 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (98.72%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -3,12 +3,25 @@ public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
         int rows = matrix.size();
         int col = matrix[0].size();
-        for(int i =0;i<rows;i++)
+        if (matrix.empty() || matrix[0].empty()) return false;
+        int start =0;
+        int end = (rows * col)-1;
+        while(start<=end)
         {
-            for(int j =0;j<col;j++)
+            int mid = start +(end - start) /2;
+            int midelement = matrix[mid/col][mid%col];
+            if(midelement == target)
             {
-                if(matrix[i][j]==target)
-                return true;
+
+             return true;
+            }
+            else if(midelement<target)
+            {
+                start = mid+1;
+            }
+            else
+            {
+                end = mid-1;
             }
         }
         return false;
