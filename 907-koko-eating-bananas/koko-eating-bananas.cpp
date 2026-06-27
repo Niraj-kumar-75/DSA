4dedab2ea7661c887499a077c5c7518623614466
niraj75 <143876990+Niraj-kumar-75@users.noreply.github.com>
2026-06-27 21:38:55 +0530

    Time: 8 ms (55.22%) | Memory: 23 MB (46.85%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.cpp
+++ 907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,48 +1,32 @@
 class Solution {
 public:
-    long long hour(vector<int>&piles,int n,int speed,int h)
+    bool ispossible(vector<int>&piles,int speed,int h)
     {
-        long long hr =0;
-        for(int i =0;i<n;i++)
+        long long totalhour = 0;
+        for(int i =0;i<piles.size();i++)
         {
-            hr=hr+piles[i]/speed;
-            if(piles[i]%speed!=0)
-            {
-                hr++;
-            }
-             if(hr > h) return hr;
+            totalhour = totalhour +(piles[i]+speed -1)/speed;
         }
-        return hr;
+        if(totalhour<=h) return true;
+        else return false;
     }
     int minEatingSpeed(vector<int>& piles, int h) {
-        int n = piles.size();
         int low = 1;
-        int res=0;
-        int high = INT_MIN;
-        int ans=0;
-        int speed =0;
-
-        for(int i =0;i<n;i++)   //maximum element nikal rhe hai from ARRAY se.
-        {
-            if(high<piles[i])
-            {
-                high = piles[i];
-            }
-        }
+        int high = *max_element(piles.begin(),piles.end());
+        int ans = high;
         while(low<=high)
         {
-            int mid = low +(high-low)/2;
-            ans = hour(piles,n,mid,h);
-            if(ans>h)
+            int mid = low + (high-low)/2;
+            if(ispossible(piles,mid,h))
             {
-                low = mid +1;
+                ans = mid;
+                high = mid-1;
             }
             else
             {
-                res = mid;
-                high = mid -1;
+                low = mid+1;
             }
         }
-        return res;
+        return ans;
     }
 };
