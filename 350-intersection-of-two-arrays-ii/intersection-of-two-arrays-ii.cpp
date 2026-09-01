e992b4b59b70e3cdf48259a10b1f32d8c977233f
niraj75 <143876990+Niraj-kumar-75@users.noreply.github.com>
2026-09-01 16:35:19 +0530

    Time: 0 ms (100.00%) | Memory: 14.4 MB (62.51%) - LeetSync

---
+++ 350-intersection-of-two-arrays-ii/intersection-of-two-arrays-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        vector<int>ans;
+        int p1 =0;
+        int p2 =0;
+        int n1=nums1.size();
+        int n2= nums2.size();
+        while(p1<n1 && p2<n2)
+        {
+            if(nums1[p1]==nums2[p2])
+            {
+                ans.push_back(nums1[p1]);
+                p1++;
+                p2++;
+            }
+            else if(nums1[p1]<nums2[p2])
+            p1++;
+            else
+            p2++;
+        }
+        return ans;
+    }
+};
