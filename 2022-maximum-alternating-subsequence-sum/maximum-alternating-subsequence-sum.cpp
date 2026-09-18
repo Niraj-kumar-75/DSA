611d0d747bf702206fe9c780e8080608a118410c
niraj75 <143876990+Niraj-kumar-75@users.noreply.github.com>
2026-09-18 20:02:26 +0530

    Time: 291 ms (21.69%) | Memory: 166.6 MB (16.48%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     int n;
     typedef long long ll;
-    long long dp[1000001][2];
-    ll solve(int idx,vector<int>&nums,bool flag)
+    // long long dp[1000001][2];
+    ll solve(int idx,vector<int>&nums,bool flag,vector<vector<ll>>&dp)
     {
         if(idx>=n)
         {
@@ -11,18 +11,19 @@ public:
         }
         if(dp[idx][flag]!=-1)
         return dp[idx][flag];
-        ll skip =solve(idx+1,nums,flag);
+        ll skip =solve(idx+1,nums,flag,dp);
         int val = nums[idx];
         if(flag==false)
         {
             val = -val;
         }
-        ll pick = solve(idx+1,nums,!flag) +val;
+        ll pick = solve(idx+1,nums,!flag,dp) +val;
         return dp[idx][flag] = max(skip,pick);
     }
     long long maxAlternatingSum(vector<int>& nums) {
         n = nums.size();
-       memset(dp,-1,sizeof(dp));
-       return solve(0,nums,true);
+    //    memset(dp,-1,sizeof(dp));
+    vector<vector<ll>>dp(n+1,vector<ll>(2,-1));
+       return solve(0,nums,true,dp);
     }
 };
